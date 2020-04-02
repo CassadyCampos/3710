5cfa10f4b4d5aeced8f91ca9f329c69ba45e1fce
Cassady <cassady801@gmail.com>
2020-04-02 15:24:35 -0600

    test'

---
+++ 3710Project/objects.hpp
@@ -0,0 +1,14 @@
+//
+//  objects.hpp
+//  3710Project
+//
+//  Created by Cassady Campos on 2020-03-29.
+//  Copyright © 2020 Cassady  Campos. All rights reserved.
+//
+
+#ifndef objects_hpp
+#define objects_hpp
+
+#include <stdio.h>
+
+#endif /* objects_hpp */
---
+++ Build/Intermediates/3710Project.build/Debug/3710Project.build/Objects-normal/x86_64/objects.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/cassadycampos/Documents/School/Spring\ 2020/CPSC\ 3710/3710Project/3710Project/objects.cpp
